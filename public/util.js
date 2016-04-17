1f70592eaa13a23605daf0661d2246adb7c807fc
lyninx <dennis.pacewicz@gmail.com>
2016-04-16 22:08:47 -0400

    add form -> json conversion and POST to api/new

---
+++ public/util.js
@@ -0,0 +1,31 @@
+console.log("hello!")
+
+$('#create-form').submit(function () {
+    // custom handling here
+    var obj = {}
+    $( "#create-form input, #create-form textarea" ).each(function( index ) {
+    	var input = $(this)
+		switch(input.attr("name")){
+			case "title":
+				obj.title = input.val()
+				break
+			case "url":
+			    obj.url = input.val()
+				break
+			case "body":
+			    obj.content = input.val()
+				break
+		}
+	});
+	console.log(obj)
+    var formData = JSON.stringify(obj)
+	$.ajax({
+	  type: "POST",
+	  url: "api/new",
+	  data: formData,
+	  success: function(){ console.log("created!")},
+	  dataType: "json",
+	  contentType : "application/json"
+	});
+    return false
+});
